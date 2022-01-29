204ccf208d520ee8ea980ed3e2a09cd496ae0797
henryvecchione <hjv@princeton.edu>
2022-01-29 16:02:09 -0500

    multiple pages working

--- webScrape.py
+++ webScrape.py
@@ -3,11 +3,22 @@ from selenium.webdriver.support import ui
 from selenium.webdriver.common.by import By
 import time
 import csv
+from datetime import date
 
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 def main():
+
+  # CLI
+  print('Tiger Inn Alumni Web Scraper')
+  print("(c) 2022 Henry Vecchione '22 (hjv@)")
+  print('Warning: Due to rate limiting on the TigerNet service, \n a limited number of profiles can be scraped per NetID per day\n\n')
+  yr = int(input('Enter the class year to search: '))
+  if yr < 1930 or yr > date.today().year:
+    print('Year must be after 1930 and before or equal to the current year')
+    return
+  print(f'Searching the class of {yr}')
 
   # attempt to open site
   web = webdriver.Chrome("/Users/henryvecchione/Desktop/Home/tiAlum/chromedriver.exe")
@@ -27,12 +38,12 @@ def main():
       print('Login successful')
       break
     except:
-      print('Not logged in', end='\r')
+      print('Select "Log in" in the browser and authenticate your NetID', end='\r')
       continue
   
   
   # once logged in, begin scraping loop
-  years = range(2021, 2022) # grad years
+  years = range(yr, yr+1) # grad years -- just one for now since it's rate limiting
   for year in years:
     print(f'Year: {year}')
     # (1) get Advanced Search site
@@ -60,64 +71,67 @@ def main():
 
     # (4) wait and get the number of results
     pageNo = 1
-    while True:
-      try:
-        numResults = web.find_element(By.XPATH, '/html/body/form/div[3]/main/section/div/div/section/div[3]/div[1]/div/div[2]/div[2]/p/strong').text
-        print(f'{numResults} members found in year {year}')
-        numPages = int(numResults) // 20 + 1 
-        break
-      except:
-        continue
-    
-    while True:
-      #(5) iterate through results
-      links = []
+    numPages = 2
+    while pageNo < int(numPages):
+      newUrl = '/'.join(url.split('/')[:-1]) + '/' + str(pageNo) + '#gridStart'
+      web.get(newUrl)
+      print(f'Page{pageNo} of {numResults}')
+      # get the number of results, calculate pages
       while True:
         try:
-          # (5.1) get all alums on a page
-          alums = web.find_elements(By.CLASS_NAME, 'imod-directory-member-more')
-          # (5.2) for each alum on page, get the link to their profile
-          for alum in alums:
-            profile = alum.find_element(By.TAG_NAME, 'a')
-            link = profile.get_attribute('href')
-            links.append(link)
+          numResults = web.find_element(By.XPATH, '/html/body/form/div[3]/main/section/div/div/section/div[3]/div[1]/div/div[2]/div[2]/p/strong').get_attribute('textContent')
+          print(f'{numResults} members found in year {year}')
+          numPages = int(numResults) // 20 + 1 
           break
-        except Exception as e:
+        except:
           continue
-
-
-      # (6) Iter through links for that page
-      for link in links:
-        # (6.1) visit site
-        web.get(link)
-        # (6.2) Get categories: personal, contact, education, career
+      
+      while True:
+        #(5) iterate through results, pulling profile links
+        links = []
         while True:
           try:
-            _ = web.find_element(By.CSS_SELECTOR, '.imod-profile-step.ng-isolate-scope.imod-profile-step-opened')
+            # (5.1) get all alums on a page
+            alums = web.find_elements(By.CLASS_NAME, 'imod-directory-member-more')
+            # (5.2) for each alum on page, get the link to their profile
+            for alum in alums:
+              profile = alum.find_element(By.TAG_NAME, 'a')
+              link = profile.get_attribute('href')
+              links.append(link)
             break
           except Exception as e:
             continue
 
-        categories = web.find_elements(By.CSS_SELECTOR, '.imod-profile-step.ng-isolate-scope.imod-profile-step-opened') + web.find_elements(By.CSS_SELECTOR, '.imod-profile-step.ng-isolate-scope.imod-profile-step-closed')
-        # (6.3) get content 
-        for c in categories:
-          fields = c.find_elements(By.CSS_SELECTOR, '.imod-profile-fields')
-          for field in fields:
-            fieldNames = field.find_elements(By.CSS_SELECTOR, '.imod-profile-field-label.ng-binding.ng-scope')
-            fieldData = field.find_elements(By.CSS_SELECTOR, '.imod-profile-field-data.ng-binding.ng-scope')
-            for n, d in zip(fieldNames, fieldData):
-              print(f'{n.get_attribute("textContent")} : {d.get_attribute("textContent")}')
-        print('*--------------------*')
-      if pageNo != numPages:
-        newUrl = '/'.join(url.split('/')[:-1]) + '/' + str(pageNo + 1) + '#gridStart'
-        web.get(newUrl)
-        print(f"Page {pageNo + 1}")
-      else:
+
+        # (6) Iterate through links for that page
+        for link in links:
+          # (6.1) visit site
+          web.get(link)
+          # (6.2) Get categories: personal, contact, education, career
+          while True:
+            try:
+              _ = web.find_element(By.CSS_SELECTOR, '.imod-profile-step.ng-isolate-scope.imod-profile-step-opened')
+              break
+            except Exception as e:
+              continue
+          
+          # get the 'categories' e.g contact info, career info, academic info
+          categories = web.find_elements(By.CSS_SELECTOR, '.imod-profile-step.ng-isolate-scope.imod-profile-step-opened') + web.find_elements(By.CSS_SELECTOR, '.imod-profile-step.ng-isolate-scope.imod-profile-step-closed')
+          # (6.3) get content 
+          for c in categories:
+            # get the fields
+            fields = c.find_elements(By.CSS_SELECTOR, '.imod-profile-fields')
+            for field in fields:
+              # get the name and data 
+              fieldNames = field.find_elements(By.CSS_SELECTOR, '.imod-profile-field-label.ng-binding.ng-scope')
+              fieldData = field.find_elements(By.CSS_SELECTOR, '.imod-profile-field-data.ng-binding.ng-scope')
+              for n, d in zip(fieldNames, fieldData):
+                print(f'{n.get_attribute("textContent")} : {d.get_attribute("textContent")}')
+          print('*--------------------*')
         break
-
-
-
-
+      # go to next page
+      pageNo += 1
+    
 
 
 
